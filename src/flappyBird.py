4c8566bb6b1cd011f7b5b49b3ccc043875698983
I341551 <ajay.pratap.singh@sap.com>
2020-01-03 16:41:02 +0530

    Restructured code

---
+++ src/flappyBird.py
@@ -0,0 +1,15 @@
+import pygame
+import time
+from random import randint,randrange
+from config import initialize
+from utils import *
+
+def main():
+    utils_main()
+    return
+
+
+if __name__ == '__main__':
+    main()
+    pygame.quit()
+    quit()
